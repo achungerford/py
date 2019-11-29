b851b20d6bff3d6e0e5bedfc9eabfd95838bc3a9
Alex Hungerford <10031447+achungerford@users.noreply.github.com>
2019-11-28 23:53:41 -0800

    pattern finding

---
+++ isPhoneNumber.py
@@ -0,0 +1,27 @@
+""" Identify a phone number pattern
+    without using regular expressions.
+"""
+
+def isPhoneNumber(text):
+    """check if string matches phone number pattern"""
+    if len(text) != 12:
+        return False
+    for i in range(0, 3):
+        if not text[i].isdecimal():
+            return False
+    if text[3] != '-':
+        return False
+    for i in range(4, 7):
+        if not text[i].isdecimal():
+            return False
+    if text[7] != '-':
+        return False
+    for i in range(8, 12):
+        if not text[i].isdecimal():
+            return False
+    return True
+
+print('415-555-4242 is a phone number:')
+print(isPhoneNumber('415-555-4242'))
+print('Moshi moshi is a phone number:')
+print(isPhoneNumber('Moshi moshi'))
